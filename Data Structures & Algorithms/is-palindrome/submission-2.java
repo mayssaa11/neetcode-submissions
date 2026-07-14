3670420ee041c181bc3ac611671e1f83923ff603
Mayssa Zennou <mayssa.zn.11@gmail.com>
2026-07-14 09:51:40 +0100

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        if (s.length()==1) return true;
+
+        // only keep alphanumerical characters and normalize everything into lowercase
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase(); 
+
+        String inverted = new StringBuilder(s).reverse().toString();
+
+        if (s.equals(inverted)) return true;
+        return false;
+    }
+}
