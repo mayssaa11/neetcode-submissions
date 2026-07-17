0170682b6766c1adaccf819b08907ad2b7d187e8
Mayssa Zennou <mayssa.zn.11@gmail.com>
2026-07-17 15:58:16 +0100

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+        int n = nums.length;
+        int[] output = new int[n];
+
+        int[] left_product = new int[n];
+        left_product[0] = 1;
+        for (int i=1; i<n; i++){
+            left_product[i] = left_product[i-1]*nums[i-1];
+        }
+
+        int[] right_product = new int[n];
+        right_product[n-1] = 1;
+        for (int i=n-2; i>=0; i--){
+            right_product[i] = right_product[i+1]*nums[i+1];
+        }
+
+        for (int i=0; i<n; i++){
+            output[i] = left_product[i]*right_product[i];
+        }
+        return output;
+
+    }
+}  
