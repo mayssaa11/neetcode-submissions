7c101bf31d7dcb6c3ff37342c61978fd193ebafc
Mayssa Zennou <mayssa.zn.11@gmail.com>
2026-08-12 11:56:48 +0100

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+
+        if (nums.length == 0) return 0;
+
+        Set<Integer> uniqueNums = new HashSet<>();        
+        for (int num : nums) {
+            uniqueNums.add(num);
+        }
+
+        int longestStreak = 0; 
+        for (Integer num: uniqueNums){
+
+            // only check against beginnings of sequences (i.e num-1 not in uniqueNums)
+            if (!uniqueNums.contains(num-1)){
+                
+                int currentStreak = 0;
+                while (true) {
+                    if (uniqueNums.contains(num++)) {
+                        currentStreak++;               
+                    } else {
+                        break; 
+                    }
+                }
+                longestStreak = Math.max(longestStreak,currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+}
