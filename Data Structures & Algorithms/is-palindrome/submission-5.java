d19ba8b12053205f195a34634b143e27d4e93435
Mayssa Zennou <mayssa.zn.11@gmail.com>
2026-08-13 09:42:51 +0100

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        int left = 0;
+        int right = s.length()-1;
+        while (left<right){
+            if (Character.isLetterOrDigit(s.charAt(left))){
+                if (Character.isLetterOrDigit(s.charAt(right))){
+                    if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)))
+                        return false;
+                    left++;
+                    right--;
+                } 
+                else right--;         
+            } 
+            else left++;
+        }
+
+        return true;
+    }
+}
